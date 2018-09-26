749000e2e95eb24ca379f194412f10f7e43d4cb2
antenorr <roudyantenor@gmail.com>
2018-09-26 17:27:35 -0400

    further setup of express api and react handshake

--- client/src/App.js
+++ client/src/App.js
@@ -3,60 +3,41 @@ import './App.css';
 
 class App extends Component {
   // Initialize state
-  state = { passwords: [] }
+  state = { 
+    passwords: []
+  };
 
   // Fetch passwords after first mount
   componentDidMount() {
     this.getPasswords();
   }
 
+
+  // Get the passwords and store them in state
   getPasswords = () => {
-    // Get the passwords and store them in state
-    fetch('/api/passwords')
-      .then(res => res.json())
-      .then(passwords => this.setState({ passwords }));
+    fetch('/api')
+      .then(response => {
+        return response.json()
+       })
+      .then( (passwords) => {
+        this.setState({ passwords })
+        console.log(passwords)
+      });
+     
   }
 
   render() {
-    const { passwords } = this.state;
-
+    // const { passwords } = this.state;
     return (
-      <div className="App">
-        {/* Render the passwords if we have them */}
-        {passwords.length ? (
-          <div>
-            <h1>5 Passwords.</h1>
-            <ul className="passwords">
-              {/*
-                Generally it's bad to use "index" as a key.
-                It's ok for this example because there will always
-                be the same number of passwords, and they never
-                change positions in the array.
-              */}
-              {passwords.map((password, index) =>
-                <li key={index}>
-                  {password}
-                </li>
-              )}
-            </ul>
+      <div> 
             <button
               className="more"
               onClick={this.getPasswords}>
               Get More
             </button>
-          </div>
-        ) : (
-            // Render a helpful message otherwise
-            <div>
-              <h1>No passwords :(</h1>
-              <button
-                className="more"
-                onClick={this.getPasswords}>
-                Try Again?
-            </button>
-            </div>
-          )}
-      </div>
+            {this.state.passwords.map(element => element.answer)}
+      </div>    
+
     );
   }
 }
--- index.js
+++ index.js
@@ -1,43 +1,19 @@
 const express = require('express');
 const path = require('path');
-const generatePassword = require('password-generator');
+//const generatePassword = require('password-generator');
 
 const app = express();
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/api/hello', (req, res) => {
-    res.json({ answer: "Hello"})
+app.get('/api', (req, res) => {
+    res.json([
+        {answer: "one"},
+        {answer: "Two"},
+        {answer: "three"}
+    ])
 })
-
-
-
-
-
-
-// Put all API endpoints under '/api'
-app.get('/api/passwords', (req, res) => {
-    const count = 5;
-
-    // Generate some passwords
-    const passwords = Array.from(Array(count).keys()).map(i =>
-        generatePassword(12, false)
-    )
-
-    // Return them as json
-    res.json(passwords);
-
-    console.log(`Sent ${count} passwords`);
-});
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-
-
-
-
-
 
 
 app.get('*', (req, res) => {
